a372994be50f030c4ff40b55c17f1c998549b142
techpool <suryadeep10@gmail.com>
2017-09-10 14:53:16 +0530

    [GROWTH] Added missing configs for Redis

--- config/main.js
+++ config/main.js
@@ -1,18 +1,26 @@
 module.exports = {
 	local: {
-		SERVICE_PORT: 3000,
-		REDIS_NOTIFY_EVENTS: 'Ex'
+		'SERVICE_PORT': 3000,
+		'REDIS_NOTIFY_EVENTS': 'Ex',
+		'REDIS_HOST': 'localhost',
+		'REDIS_PORT': 6379
 	},
 	devo: {
-		SERVICE_PORT: 80,
-		REDIS_NOTIFY_EVENTS: 'Ex'
+		'SERVICE_PORT': 80,
+		'REDIS_NOTIFY_EVENTS': 'Ex',
+		'REDIS_HOST': 'ecs-growth.cpzshl.0001.apse1.cache.amazonaws.com',
+		'REDIS_PORT': 8080,
 	},
 	gamma: {
-		SERVICE_PORT: 80,
-		REDIS_NOTIFY_EVENTS: 'Ex'
+		'SERVICE_PORT': 80,
+		'REDIS_NOTIFY_EVENTS': 'Ex',
+		'REDIS_HOST': 'ecs-growth.cpzshl.0001.apse1.cache.amazonaws.com',
+		'REDIS_PORT': 8080,
 	},
 	prod: {
-		SERVICE_PORT: 80,
-		REDIS_NOTIFY_EVENTS: 'Ex'
+		'SERVICE_PORT': 80,
+		'REDIS_NOTIFY_EVENTS': 'Ex',
+		'REDIS_HOST': 'ecs-growth.cpzshl.0001.apse1.cache.amazonaws.com',
+		'REDIS_PORT': 8080,
 	}
 }
