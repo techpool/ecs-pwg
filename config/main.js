df9bea627058f17e93773ca39de3589a73a18761
Raghu Raman <rraagghhu@gmail.com>
2017-12-14 14:29:43 +0530

    Updating redis config.

--- config/main.js
+++ config/main.js
@@ -10,9 +10,9 @@ module.exports = {
 	devo: {
 		'SERVICE_PORT': 80,
 		'REDIS_NOTIFY_EVENTS': 'Ex',
-		'REDIS_HOST': 'growth-ecs.e6ocw5.0001.apse1.cache.amazonaws.com',
+		'REDIS_HOST': 'devo-ecs.e6ocw5.0001.apse1.cache.amazonaws.com',
 		'REDIS_PORT': 8080,
-		'REDIS_DB': 0,
+		'REDIS_DB': 10,
 		'PWG_LOAD_BALANCER': process.env.API_END_POINT
 	},
 	gamma: {
