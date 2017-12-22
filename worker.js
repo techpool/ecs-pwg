ed4da7b62591aae1e469d5fd6cb04a4f7624841b
Raghu Raman <rraagghhu@gmail.com>
2017-12-22 10:43:26 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -10,14 +10,15 @@ pubsub.psubscribe(stageConfig.REDIS.NOTIFY);
 
 pubsub.on('pmessage', (pattern, channel, message) => {
 	if (channel === `__keyevent@${stageConfig.REDIS.DB}__:expired`) {
-        let x = message.split('|');
-        if (x[0] === 'shadow') {
-            const key = 'key' + message.substr('shadow'.length);
-            pwgUtil.deleteBucketAllocation(key)
-                .then(() => console.log(`INFO :: WORKER :: DELETE :: ${key}`))
-                .catch(() => console.error(`ERROR :: WORKER :: DELETE :: ${key}`));
-        }
+		let x = message.split('|');
+		if (x[0] === 'shadow') {
+			const key = 'key' + message.substr('shadow'.length);
+			pwgUtil.deleteBucketAllocation(key)
+				.then(() => console.log(`INFO :: WORKER :: DELETE :: ${key}`))
+				.catch(() => console.error(`ERROR :: WORKER :: DELETE :: ${key}`))
+			;
+		}
 	}
 });
 
-pubsub.on('psubscribe', (e) => console.log( `Redis pubsub has subscribed successfully to event: ${e}` ));
+pubsub.on('psubscribe', (e) => console.log(`Redis pubsub has subscribed successfully to event: ${e}`));
