78e9f185e0772c69fadce6165f012fc2cc07592c
Raghu Raman <rraagghhu@gmail.com>
2017-12-22 08:44:11 +0530

    Bug fixes.

--- src/filter/crawler.js
+++ src/filter/crawler.js
@@ -68,7 +68,7 @@ router.get('/*', (req, res, next) => {
 
     if (isCrawler)
         return pipeUtil.pipeToMiniP(req, res, {
-            headers: {'Access-Token': '5deb8b92-f406-4a5c-ad8b-db2417461b70'} // TODO: Remove hack
+            headers: Object.assign(req.headers, {'Access-Token': '5deb8b92-f406-4a5c-ad8b-db2417461b70'}) // TODO: Remove hack
         });
 
     return next();
--- src/util/common/pipe.js
+++ src/util/common/pipe.js
@@ -8,21 +8,34 @@ const PipeUtil = function() {
 
     const self = this;
 
-    self.pipe = (req, res, options) =>
-        req.pipe(request({
-            uri: options.uri || options.url || stageConfig.ECS_ENDPOINT,
-            method: options.method || req.method,
-            qs: options.query || req.query || {},
-            headers: options.headers || req.headers || {},
+    self.pipe = (req, res, options) => {
+
+        const 
+            uri = options.uri || options.url || stageConfig.ECS_ENDPOINT,
+            method = options.method || req.method,
+            qs = options.query || req.query || {},
+            headers = options.headers || req.headers || {};
+
+        // Debugging
+        console.log(`PIPE_REQUEST :: ${uri} :: ${method} :: ${JSON.stringify(qs)} :: ${JSON.stringify(headers)}`);
+
+        return req.pipe(request({
+            uri: uri,
+            method: method,
+            qs: qs,
+            headers: headers,
             followAllRedirects: false,
             followRedirect: false,
             jar: true
         }))
         .on('response', (response) => {
-            console.log(`PIPE :: ${port} :: ${response.statusCode} :: ${JSON.stringify(response.headers)}`);
+            console.log(`PIPE_RESPONSE :: ${uri} :: ${method} :: ${response.statusCode} :: ${JSON.stringify(response.headers)}`);
             res.writeHead(response.statusCode, response.headers);
         })
         .pipe(res);
+
+    };
+
 
     self.pipeToMiniG = (req, res, options) =>
         self.pipe(req, res, Object.assign({}, options, {'uri': stageConfig.ECS_ENDPOINT + ':81' + req.path}));
