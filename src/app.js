327deeccf1ce548f1bcb7d433dae5070fb5411a0
Raghu Raman <rraagghhu@gmail.com>
2017-12-22 13:37:49 +0530

    Fix formatting.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const
     express = require('express'),
     morgan = require('morgan'),
-	cookieParser = require('cookie-parser'),
+    cookieParser = require('cookie-parser'),
     compression = require('compression');
 
 const
