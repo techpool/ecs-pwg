1ce4f55b7c034d3e2c171d76abc848514327dd37
Raghu Raman <rraagghhu@gmail.com>
2017-12-28 19:28:20 +0530

    logs

--- worker.js
+++ worker.js
@@ -9,6 +9,11 @@ const pubsub = require('./src/util/common/redis')['pubsub'];
 pubsub.psubscribe(stageConfig.REDIS.NOTIFY);
 
 pubsub.on('pmessage', (pattern, channel, message) => {
+	console.log('----pmessage----')
+	console.log(`pattern: ${pattern}`)
+	console.log(`channel: ${channel}`)
+	console.log(`message: ${message}`)
+	console.log('----pmessage----')
 	if (channel === `__keyevent@${stageConfig.REDIS.DB}__:expired`) {
 		console.log(`INFO :: REDIS_EXPIRED :: ${message}`);
 		let x = message.split('|');
@@ -25,4 +30,4 @@ pubsub.on('pmessage', (pattern, channel, message) => {
 pubsub.on('psubscribe', (e) => console.log(`Redis pubsub has subscribed successfully to event: ${e}`));
 
 console.log('PWG worker started...');
-console.log('Thi service is responsible for cleaning up the expired access tokens, which are assigned to buckets!');
+console.log('This service is responsible for cleaning up the expired access tokens, which are assigned to buckets!');
