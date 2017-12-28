f07a8676b4cae1f0a9b2fbc8e04d222e0ba78598
Raghu Raman <rraagghhu@gmail.com>
2017-12-28 19:59:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const
 	stageConfig = require('./src/config/stage');
 
 app.listen(stageConfig.PORT, ()=> console.log(`Server running on port ${stageConfig.PORT}`));
+
+
+// Subscribing to redis pubsub events
+const pubsub = require('./src/util/common/redis')['pubsub'];
+pubsub.psubscribe(stageConfig.REDIS.NOTIFY);
+
+pubsub.on('pmessage', (pattern, channel, message) => {
+	console.log('----pmessage----')
+	console.log(`pattern: ${pattern}`)
+	console.log(`channel: ${channel}`)
+	console.log(`message: ${message}`)
+	console.log('----pmessage----')
+});
+
+pubsub.on('psubscribe', (e) => console.log(`Redis pubsub has subscribed successfully to event: ${e}`));
