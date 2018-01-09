eabada655a0e1ea10a4e99812728844d0350bfa7
Raghu Raman <rraagghhu@gmail.com>
2018-01-09 10:27:12 +0530

    Thread pool implementation.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # start nginx
-# exec service nginx start --with-threads &
-exec service nginx start &
+exec service nginx start --with-threads &
+# exec service nginx start &
 
 # start node server
 exec npm run-script server
