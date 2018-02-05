7e13f195e37b1c6245385df5258bd0182decb96d
Raghu Raman <rraagghhu@gmail.com>
2018-02-05 13:35:40 +0530

    Logging poc.

--- src/app.js
+++ src/app.js
@@ -74,13 +74,32 @@ app.get('/poc3', (req, res, next) => {
 });
 
 app.get('/poc4', (req, res, next) => {
-	let len = req.query.len ? parseInt(req.query.len) : 1;
-	if (len > 1000000) len = 1000000;
-	let x = Date.now();
-	for(let i = 0; i < len; i++) console.log("This is a sample log...");
+    // len
+    let len = req.query.len ? parseInt(req.query.len) : 1;
+    if (len > 1000000) len = 1000000;
+
+    // message
+    let message = "This is a sample log...";
+    if (req.query.message) message = req.query.message;
+    try {
+        message = JSON.parse(decodeURIComponent(message));
+    } catch(e) {
+        // do nothing
+        console.log('could not parse json');
+    }
+
+    // stringify
+    let stringify = false;
+    if (req.query.stringify === "true") stringify = true;
+    if (stringify && typeof(message) === 'object') message = JSON.stringify(message);
+
+    // time it
+    let x = Date.now();
+	for(let i = 0; i < len; i++) console.log(message);
 	let y = Date.now();
 	console.log(`Time taken to print ${len} logs = ${y-x} ms`);
-	res.json({time: `${y-x} ms`, len: `${len}`});
+    res.json({time: `${y-x} ms`, len: `${len}`});
+
 });
 /* ----------------------- POC ----------------------- */
 
